158790d5414baf5a7aa8dbaac8e4f53588d2a9f7
Haicheng Chen <hanseychen@gmail.com>
2019-01-22 11:56:26 -0500

    Implement the server thread

--- vector-clock/java/TCPServer.java
+++ vector-clock/java/TCPServer.java
@@ -2,30 +2,50 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+
+import com.sun.corba.se.impl.ior.ByteBuffer;
 
 public class TCPServer {
-  private static void log(String content) {
-    System.out.print("[Server] " + content + "\n");
-  }
-  private static void err(String content) {
-    System.err.print("[Server] " + content + "\n");
-  }
-  public static void main(String args[]) {
-    // Simple sanity check.
-    if (args.length != 1) {
-      err("Usage: TCPServer <port>");
-      System.exit(1);
-    }
-    // Get server port.
-    int port = Integer.parseInt(args[0]);
+  private String nodeId;
+  private int listeningPort;
+  private int messageCount;
 
+  public TCPServer(String id, int port, int count) {
+    nodeId = id;
+    listeningPort = port;
+    messageCount = count;
+  }
+
+  private void log(String content) {
+    System.out.print("[server@" + nodeId + "] " + content + "\n");
+  }
+  private void err(String content) {
+    System.err.print("[server@" + nodeId + "] " + content + "\n");
+  }
+
+  public void run() {
     // Start the server.
-    try (ServerSocket listener = new ServerSocket(port)) {
-      while (true) {
+    try (ServerSocket listener = new ServerSocket(listeningPort)) {
+      for (int msgIdx = 0; msgIdx < messageCount; ++msgIdx) {
+        // Read one message.
         try (Socket socket = listener.accept()) {
+          // Increment the vector clock when we receive something.
+          VectorClock.increment(nodeId);
+
+          // Receive the remote vector clock.
           DataInputStream in = new DataInputStream(socket.getInputStream());
-          int data = in.readInt();
-          log("Read number " + data + " from a client.");
+          int messageSize = in.readInt();
+          ByteBuffer buf = ByteBuffer.allocate(messageSize);
+          for (int byteIdx = 0; byteIdx < messageSize; ++ byteIdx) {
+            buf.put(in.readByte());
+          }
+          HashMap<String, Integer> remoteClock = VectorClock.deserialize(buf);
+          log("Received " + VectorClock.stringVerionOf(remoteClock));
+
+          // Merge the vector clock.
+          VectorClock.merge(remoteClock);
+          log("Current vector clock is " + VectorClock.stringVersion());
         }
       }
     } catch (IOException e) {
